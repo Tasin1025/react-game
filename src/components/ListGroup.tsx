bf1d73a7002923c7f905a0603fdda7885ca960b3
Tasin1025 <tasintt@gmail.com>
2024-06-19 03:49:32 +0600

    city list added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,16 @@
 function ListGroup() {
-    
+  const cities = ["dhaka", "chittagong", "rajshahi", "sylhet", "barishal"];
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <ul className="list-group">
+        {cities.map((city) => (
+          <li>{city}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
+
+export default ListGroup;
